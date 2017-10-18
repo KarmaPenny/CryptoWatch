a5cba7356c87b4e3ccd1f84ce6fd850d5aaab570
Cole Robinette <cole.robinette@ge.com>
2017-10-18 17:41:42 -0400

    fixed bug with copying donate address

--- app/src/main/java/com/colerobinette/cryptowatch/Donate.java
+++ app/src/main/java/com/colerobinette/cryptowatch/Donate.java
@@ -4,19 +4,20 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Donate extends AppCompatActivity {
-    public void CopyBTC() {
+    public void CopyBTC(View v) {
         ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
         clipboard.setPrimaryClip(ClipData.newPlainText("donate_btc", "3KpH3d7eBwWNihMj9Z64qNG1sjj1mLFfom"));
     }
 
-    public void CopyLTC() {
+    public void CopyLTC(View v) {
         ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
         clipboard.setPrimaryClip(ClipData.newPlainText("donate_btc", "LRAXnHBzDwjQpaebBykoK87WB8XtMsEtD8"));
     }
 
-    public void CopyETH() {
+    public void CopyETH(View v) {
         ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
         clipboard.setPrimaryClip(ClipData.newPlainText("donate_btc", "0x013f07D2ad0dbA2F67800b223f78a56E616961F9"));
     }
